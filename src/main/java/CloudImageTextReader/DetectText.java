40586eac62077e9b15f6d6e304ba6ed190dcb26c
Zoli <toth_zoltan@tolna.net>
2021-05-14 18:24:32 +0200

    Another type of DetectText

--- src/main/java/CloudImageTextReader/DetectText.java
+++ src/main/java/CloudImageTextReader/DetectText.java
@@ -19,9 +19,38 @@ public class DetectText {
                 AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
         requests.add(request);
 
-        // Initialize client that will be used to send requests. This client only needs to be created
-        // once, and can be reused for multiple requests. After completing all of your requests, call
-        // the "close" method on the client to safely clean up any remaining background resources.
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.format("Error: %s%n", res.getError().getMessage());
+                    Texts.add("Error");
+                }
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+                    Texts.add(annotation.getDescription());
+                    System.out.format("Text: %s%n", annotation.getDescription());
+                    System.out.format("Position : %s%n", annotation.getBoundingPoly());
+                }
+            }
+        }
+        return Texts;
+    }
+    public static ArrayList<String> detectText(String bucket, String filename) throws IOException {
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+        ArrayList<String> Texts= new ArrayList<>() ;
+        String filePath=String.format("gs://%s/%s",bucket,filename);
+        ImageSource imgSource = ImageSource.newBuilder().setGcsImageUri(filePath).build();
+        Image img = Image.newBuilder().setSource(imgSource).build();
+
+        Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+
         try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
             BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
             List<AnnotateImageResponse> responses = response.getResponsesList();
--- src/main/java/CloudImageTextReader/FileTrigger.java
+++ src/main/java/CloudImageTextReader/FileTrigger.java
@@ -19,7 +19,9 @@ private static final Logger logger = Logger.getLogger(FileTrigger.class.getName(
         try {
             String Path=String.format("gs://%s/%s",event.getBucket(),event.getName());
 logger.info("FileName: "+Path);
-            Texts= detectText(Path);
+
+           // Texts= detectText(Path);
+            Texts=detectText(event.getBucket(),event.getName());
             String dbUser="root";
             String dbPassword="Cl0ud2021!";
             String dbName="Trainsdb";
